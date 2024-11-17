490cfb410d1d8ff675eb034cdb2fb9ff14322365
Mike Joseph <mjoseph@iadev.net>
2024-11-17 12:54:57 -0800

    Added a button to invoke a test of a secured endpoint.

--- ng-app/src/app/home/home.component.ts
+++ ng-app/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CardMenuItem } from '../shared/card-menu/card-menu.component';
+import { AppConfig } from '../_api';
+import { APP_CONFIG } from '../core/_services/config-injection';
+import { RestService } from '../core/_services/rest.service';
+import { LoggerService } from '../core/_services/logger.service';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +18,23 @@ export class HomeComponent {
       icon: 'fa fa-home',
     },
   ];
+
+  constructor(
+    @Inject(APP_CONFIG) public config: AppConfig,
+    private logger: LoggerService,
+    private rest: RestService
+  ) {}
+
+  testSecureEndpoint() {
+    this.rest.getResource('GetColorList').subscribe(
+      (data) => {
+        console.log('Success', data);
+        this.logger.success('That endpoint worked!');
+      },
+      (err) => {
+        console.log('Error', err);
+        this.logger.error('That endpoint threw an error.');
+      }
+    );
+  }
 }
