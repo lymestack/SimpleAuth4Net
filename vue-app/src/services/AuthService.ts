67c90f95bc9a8391b74f022772fd934b76d7f88b
Mike Joseph <mjoseph@iadev.net>
2024-12-07 10:32:06 -0800

    bug fix.

--- vue-app/src/services/AuthService.ts
+++ vue-app/src/services/AuthService.ts
@@ -12,8 +12,8 @@ export interface LoginWithGoogleModel {
 }
 
 class AuthService {
-  // private apiUrl: string = 'http://localhost/SimpleAuthNet/api/'; <-- Use this for IIS
-  private apiUrl: string = "http://localhost:5218/";
+  // private apiUrl: string = 'http://localhost/SimpleAuthNet/api/Auth/'; <-- Use this for IIS
+  private apiUrl: string = "http://localhost:5218/Auth/";
   private deviceId: string = this.getOrGenerateDeviceId();
 
   // Store token expiration in local storage
