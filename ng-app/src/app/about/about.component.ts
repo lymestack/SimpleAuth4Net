319d4f5809614e3f8b23dd0854db93b5d2b92711
Mike Joseph <mjoseph@iadev.net>
2024-12-29 10:09:05 -0800

    Added a button to the about page that will clear the MFA preference value from local storage (if it exists).

--- ng-app/src/app/about/about.component.ts
+++ ng-app/src/app/about/about.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoggerService } from '../core/_services/logger.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  preferredMfaMethodExists: boolean = false;
 
+  constructor(private logger: LoggerService) {}
+
+  ngOnInit(): void {
+    // Check if the "preferredMfaMethod" variable exists in local storage
+    this.preferredMfaMethodExists =
+      !!localStorage.getItem('preferredMfaMethod');
+  }
+
+  clearMfaPreference(): void {
+    const confirmation = confirm(
+      'Are you sure you want to clear your MFA preference?'
+    );
+    if (confirmation) {
+      localStorage.removeItem('preferredMfaMethod');
+      this.preferredMfaMethodExists = false;
+      this.logger.success('MFA preference cleared successfully.');
+    }
+  }
 }
